cea8c0819484a0e0b187a15de61bba76f81392c1
Lucian Caetano <lucian.santos.caetano@gmail.com>
2022-05-04 04:40:34 -0300

    ✨ update protocol

--- src/main.cpp
+++ src/main.cpp
@@ -113,24 +113,6 @@ void loop() {
   delay(50);
 }
 
-#define BTN_L1 0
-#define BTN_L2 1
-#define BTN_R1 2
-#define BTN_R2 3
-#define BTN_L3 4
-#define BTN_R3 5
-#define BTN_UP 6
-#define BTN_DOWN 7
-#define BTN_LEFT 8
-#define BTN_RIGHT 9
-#define BTN_TRIANGLE 10
-#define BTN_CROSS 11
-#define BTN_SQUARE 12
-#define BTN_CIRCLE 13
-#define BTN_SELECT 14
-#define BTN_START 15
-
-
 void readPSX() {
   if(PSXerror > 0) return;
 
