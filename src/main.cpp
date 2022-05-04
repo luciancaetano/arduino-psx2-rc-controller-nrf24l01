21f14b16bc5a4757201ff8a567976268f8b60524
Lucian Caetano <lucian.santos.caetano@gmail.com>
2022-05-04 04:15:56 -0300

    🐛 fix some data types and problemns with digital buttons

--- src/main.cpp
+++ src/main.cpp
@@ -61,7 +61,7 @@ void setup() {
   channel |= digitalRead(CHAN_PIN_BIT_8) << 7;
 
 
-  PSXerror = ps2x.config_gamepad(PS2_CLK_PIN, PS2_CMD_PIN, PS2_ATT_PIN, PS2_DATA_PIN, true, true);
+  PSXerror = ps2x.config_gamepad(PS2_CLK_PIN, PS2_CMD_PIN, PS2_ATT_PIN, PS2_DATA_PIN, false, false);
 
   if(PSXerror != 0) {
     while(true){
@@ -95,6 +95,11 @@ void setup() {
 	radio.setPALevel(RF24_PA_MAX);
 	radio.stopListening();
 
+  // play two beeps
+  tone(BUZZER_PIN, 1000);
+  delay(100);
+  noTone(BUZZER_PIN);
+  delay(100);
   tone(BUZZER_PIN, 1000);
   delay(100);
   noTone(BUZZER_PIN);
@@ -105,38 +110,38 @@ void loop() {
 
   radio.write(&keyValues, sizeof(keyValues));
 
-  delay(10);
+  delay(50);
 }
 
 void readPSX() {
-  if(PSXerror == 1) return;
+  if(PSXerror > 0) return;
 
-  ps2x.read_gamepad(false, false);
-
-
-  keyValues.digitalKeys.START = ps2x.Button(PSB_START);
-  keyValues.digitalKeys.SELECT = ps2x.Button(PSB_SELECT);
-
-  keyValues.digitalKeys.CIRCLE = ps2x.Button(PSB_CIRCLE);
-  keyValues.digitalKeys.CROSS = ps2x.Button(PSB_CROSS);
-  keyValues.digitalKeys.SQUARE = ps2x.Button(PSB_SQUARE);
-  keyValues.digitalKeys.TRIANGLE = ps2x.Button(PSB_TRIANGLE);
-
-  keyValues.digitalKeys.LEFT = ps2x.Button(PSB_PAD_LEFT);
-  keyValues.digitalKeys.RIGHT = ps2x.Button(PSB_PAD_RIGHT);
-  keyValues.digitalKeys.UP = ps2x.Button(PSB_PAD_UP);
-  keyValues.digitalKeys.DOWN = ps2x.Button(PSB_PAD_DOWN);
-
-  keyValues.digitalKeys.L1 = ps2x.Button(PSB_L1);
-  keyValues.digitalKeys.L2 = ps2x.Button(PSB_L2);
-  keyValues.digitalKeys.R1 = ps2x.Button(PSB_R1);
-  keyValues.digitalKeys.R2 = ps2x.Button(PSB_R2);
-  keyValues.digitalKeys.L3 = ps2x.Button(PSB_L3);
-  keyValues.digitalKeys.R3 = ps2x.Button(PSB_R3);
+  ps2x.read_gamepad();
 
   keyValues.analogKeys.LX = ps2x.Analog(PSS_LX);
   keyValues.analogKeys.LY = ps2x.Analog(PSS_LY);
 
   keyValues.analogKeys.RX = ps2x.Analog(PSS_RX);
   keyValues.analogKeys.RY = ps2x.Analog(PSS_RY);
+
+  keyValues.digitalKeys.LEFT = ps2x.Button(PSB_PAD_LEFT) ? 'f' : 't';
+  keyValues.digitalKeys.RIGHT = ps2x.Button(PSB_PAD_RIGHT) ? 'f' : 't';
+  keyValues.digitalKeys.UP = ps2x.Button(PSB_PAD_UP) ? 'f' : 't';
+  keyValues.digitalKeys.DOWN = ps2x.Button(PSB_PAD_DOWN) ? 'f' : 't';
+
+  keyValues.digitalKeys.START = ps2x.Button(PSB_START) ? 'f' : 't';
+  keyValues.digitalKeys.SELECT = ps2x.Button(PSB_SELECT) ? 'f' : 't';
+
+  keyValues.digitalKeys.CIRCLE = ps2x.Button(PSB_CIRCLE) ? 'f' : 't';
+  keyValues.digitalKeys.CROSS = ps2x.Button(PSB_CROSS) ? 'f' : 't';
+  keyValues.digitalKeys.SQUARE = ps2x.Button(PSB_SQUARE) ? 'f' : 't';
+  keyValues.digitalKeys.TRIANGLE = ps2x.Button(PSB_TRIANGLE) ? 'f' : 't';
+
+  keyValues.digitalKeys.L1 = ps2x.Button(PSB_L1) ? 'f' : 't';
+  keyValues.digitalKeys.L2 = ps2x.Button(PSB_L2) ? 'f' : 't';
+  keyValues.digitalKeys.R1 = ps2x.Button(PSB_R1) ? 'f' : 't';
+  keyValues.digitalKeys.R2 = ps2x.Button(PSB_R2) ? 'f' : 't';
+  keyValues.digitalKeys.L3 = ps2x.Button(PSB_L3) ? 'f' : 't';
+  keyValues.digitalKeys.R3 = ps2x.Button(PSB_R3) ? 'f' : 't';
+
 }
--- src/types.h
+++ src/types.h
@@ -6,22 +6,22 @@ typedef struct Analogs {
 };
 
 typedef union Digitals{
-	unsigned int L1 : 1;
-	unsigned int L2 : 1;
-	unsigned int R1 : 1;
-	unsigned int R2 : 1;
-	unsigned int L3 : 1;
-	unsigned int R3 : 1;
-	unsigned int UP : 1;
-	unsigned int DOWN : 1;
-	unsigned int LEFT : 1;
-	unsigned int RIGHT : 1;
-	unsigned int TRIANGLE : 1;
-	unsigned int CROSS : 1;
-	unsigned int SQUARE : 1;
-	unsigned int CIRCLE : 1;
-	unsigned int SELECT : 1;
-	unsigned int START : 1;
+	unsigned char L1 : 1;
+	unsigned char L2 : 1;
+	unsigned char R1 : 1;
+	unsigned char R2 : 1;
+	unsigned char L3 : 1;
+	unsigned char R3 : 1;
+	unsigned char UP : 1;
+	unsigned char DOWN : 1;
+	unsigned char LEFT : 1;
+	unsigned char RIGHT : 1;
+	unsigned char TRIANGLE : 1;
+	unsigned char CROSS : 1;
+	unsigned char SQUARE : 1;
+	unsigned char CIRCLE : 1;
+	unsigned char SELECT : 1;
+	unsigned char START : 1;
 } ;
 
 struct PS2Keys {
